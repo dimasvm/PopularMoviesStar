31e330df63eaaecccaa87e22e7f5e9397883af52
Dimas Maulana <araymaulana66@gmail.com>
2018-02-03 09:20:38 +0700

    add support fragment

--- app/src/main/java/com/example/dimas/popular_movies_star/ui/activity/DetailMovieActivity.java
+++ app/src/main/java/com/example/dimas/popular_movies_star/ui/activity/DetailMovieActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,6 +26,7 @@ import com.example.dimas.popular_movies_star.mvp.presenter.DetailPresenter;
 import com.example.dimas.popular_movies_star.mvp.presenter.DetailPresenterImp;
 import com.example.dimas.popular_movies_star.mvp.view.DetailMovieView;
 import com.example.dimas.popular_movies_star.ui.adapter.TrailerAdapter;
+import com.example.dimas.popular_movies_star.ui.fragment.DetailMovieFragment;
 import com.example.dimas.popular_movies_star.utils.Constants;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
@@ -40,19 +42,9 @@ import static com.example.dimas.popular_movies_star.ui.activity.MainActivity.EXT
  */
 
 public class DetailMovieActivity
-        extends AppCompatActivity implements DetailMovieView {
+        extends AppCompatActivity {
 
-    private ImageView imageBackdrop;
-    private ImageView imagePoster;
-    private TextView titleMovie;
-    private TextView ratingMovie;
-    private TextView releaseDate;
-    private TextView overviewMovie;
-
-    private Movie movie;
     Bundle bundle;
-    DetailPresenter detailPresenter;
-    List<Trailer> trailerList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,112 +54,19 @@ public class DetailMovieActivity
 
         bundle = getIntent().getExtras();
 
-        if (bundle != null) {
-            movie = bundle.getParcelable(EXTRA_MOVIE);
+
+
+        if (savedInstanceState == null){
+
+            DetailMovieFragment detailMovieFragment = new DetailMovieFragment();
+            detailMovieFragment.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_detail_movie_fragment, detailMovieFragment)
+                    .commit();
         }
 
-        detailPresenter = new DetailPresenterImp(this, this);
-
-        initViews();
-
-        showDetailMovie();
-
     }
 
-    private void initViews() {
-        Toolbar toolbar = findViewById(R.id.toolbar_detail);
-        setSupportActionBar(toolbar);
-
-        if (getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-        toolbar.setNavigationOnClickListener(view -> finish());
-
-        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar_layout);
-        collapsingToolbarLayout.setTitle(movie.getTitle());
-        collapsingToolbarLayout.setCollapsedTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
-
-        imageBackdrop = findViewById(R.id.backdrop_image);
-        imagePoster = findViewById(R.id.poster_image);
-
-        titleMovie = findViewById(R.id.title_movie);
-        ratingMovie = findViewById(R.id.rating_movie);
-        releaseDate = findViewById(R.id.release_date);
-        overviewMovie = findViewById(R.id.overview_text);
-
-    }
-
-    @Override
-    public void showDetailMovie() {
-        if (movie != null) {
-            showLoading(false);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                String imageMovieTransitionName = bundle.getString(EXTRA_IMAGE_MOVIE_TRANSITION_NAME);
-                imagePoster.setTransitionName(imageMovieTransitionName);
-            }
-            Picasso.with(this)
-                    .load(movie.getBackdrop_path())
-                    .into(imageBackdrop, new Callback() {
-                        @Override
-                        public void onSuccess() {
-                            supportStartPostponedEnterTransition();
-                        }
-
-                        @Override
-                        public void onError() {
-                            supportStartPostponedEnterTransition();
-                        }
-                    });
-            Picasso.with(this).load(movie.getPoster_path()).into(imagePoster);
-            titleMovie.setText(movie.getTitle());
-            String release = getResources().getString(R.string.release_date) + movie.getRelease_date();
-            ratingMovie.setText(String.valueOf(movie.getVote_average()));
-            releaseDate.setText(release);
-            overviewMovie.setText(movie.getOverview());
-
-            detailPresenter.setupTrailerVideo(movie.getId());
-
-        } else {
-            showError("Movie gagal di load");
-        }
-    }
-
-    @Override
-    public void showTrailer(List<Trailer> listT) {
-        RecyclerView recyclerView = findViewById(R.id.recyclerView_trailer);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        // TODO : Set Adapter recyclerview trailer
-        TrailerAdapter trailerAdapter = new TrailerAdapter(this, listT);
-        // todo set listener trailer adapter
-        trailerAdapter.setOnTrailerClickListener(position -> {
-            Trailer trailer = trailerAdapter.getItem(position);
-            if (trailer != null){
-                Intent intent = null;
-                try {
-                    intent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(Constants.APIConstants.BASE_YOUTUBE_URL + trailer.getKey()));
-                } catch (ActivityNotFoundException ex){
-                    showError(ex.getMessage());
-                }
-                startActivity(intent);
-            }
-        });
-        recyclerView.setAdapter(trailerAdapter);
-        recyclerView.setHasFixedSize(true);
-    }
-
-
-    @Override
-    public void showLoading(boolean b) {
-
-    }
-
-    @Override
-    public void showError(String errorMessage) {
-        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
-    }
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/com/example/dimas/popular_movies_star/ui/fragment/DetailMovieFragment.java
@@ -0,0 +1,183 @@
+package com.example.dimas.popular_movies_star.ui.fragment;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dimas.popular_movies_star.R;
+import com.example.dimas.popular_movies_star.data.model.Movie;
+import com.example.dimas.popular_movies_star.data.model.Trailer;
+import com.example.dimas.popular_movies_star.mvp.presenter.DetailPresenter;
+import com.example.dimas.popular_movies_star.mvp.presenter.DetailPresenterImp;
+import com.example.dimas.popular_movies_star.mvp.view.DetailMovieView;
+import com.example.dimas.popular_movies_star.ui.activity.MainActivity;
+import com.example.dimas.popular_movies_star.ui.adapter.TrailerAdapter;
+import com.example.dimas.popular_movies_star.utils.Constants;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import static com.example.dimas.popular_movies_star.ui.activity.MainActivity.EXTRA_IMAGE_MOVIE_TRANSITION_NAME;
+
+/**
+ * Created by       : dimas on 02/02/18.
+ * Email            : araymaulana66@gmail.com
+ */
+
+public class DetailMovieFragment extends Fragment implements DetailMovieView{
+
+    AppCompatActivity activity;
+
+    ImageView imageBackdrop;
+    ImageView imagePoster;
+    TextView titleMovie;
+    TextView ratingMovie;
+    TextView releaseDate;
+    TextView overviewMovie;
+
+    Movie movie;
+    Bundle bundle;
+
+    DetailPresenter detailPresenter;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bundle = this.getArguments();
+
+        if (bundle != null){
+            movie = bundle.getParcelable(MainActivity.EXTRA_MOVIE);
+        }
+
+        activity = ((AppCompatActivity) getActivity());
+        detailPresenter = new DetailPresenterImp(activity, this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail_movie, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        initViews(view);
+        showDetailMovie();
+    }
+
+    private void initViews(View view){
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar_detail);
+        activity.setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(view1 -> activity.finish());
+
+
+        if (activity.getSupportActionBar() != null){
+            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            activity.getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        CollapsingToolbarLayout collapsingToolbarLayout = view.findViewById(R.id.collapsing_toolbar_layout);
+        collapsingToolbarLayout.setTitle(movie.getTitle());
+        collapsingToolbarLayout.setCollapsedTitleTextColor(ContextCompat.getColor(activity, android.R.color.white));
+
+        imageBackdrop = view.findViewById(R.id.backdrop_image);
+        imagePoster = view.findViewById(R.id.poster_image);
+
+        titleMovie = view.findViewById(R.id.title_movie);
+        ratingMovie = view.findViewById(R.id.rating_movie);
+        releaseDate = view.findViewById(R.id.release_date);
+        overviewMovie = view.findViewById(R.id.overview_text);
+    }
+
+    @Override
+    public void showDetailMovie() {
+        if (movie != null) {
+            showLoading(false);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                String imageMovieTransitionName = bundle.getString(EXTRA_IMAGE_MOVIE_TRANSITION_NAME);
+                imagePoster.setTransitionName(imageMovieTransitionName);
+            }
+            Picasso.with(activity)
+                    .load(movie.getBackdrop_path())
+                    .into(imageBackdrop, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            activity.supportStartPostponedEnterTransition();
+                        }
+
+                        @Override
+                        public void onError() {
+                            activity.supportStartPostponedEnterTransition();
+                        }
+                    });
+            Picasso.with(activity).load(movie.getPoster_path()).into(imagePoster);
+            titleMovie.setText(movie.getTitle());
+            String release = getResources().getString(R.string.release_date) + movie.getRelease_date();
+            ratingMovie.setText(String.valueOf(movie.getVote_average()));
+            releaseDate.setText(release);
+            overviewMovie.setText(movie.getOverview());
+
+            detailPresenter.setupTrailerVideo(movie.getId());
+
+        } else {
+            showError("Movie gagal di load");
+        }
+    }
+
+    @Override
+    public void showTrailer(List<Trailer> listT) {
+        RecyclerView recyclerView = activity.findViewById(R.id.recyclerView_trailer);
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false));
+        // TODO : Set Adapter recyclerview trailer
+        TrailerAdapter trailerAdapter = new TrailerAdapter(activity, listT);
+        // todo set listener trailer adapter
+        trailerAdapter.setOnTrailerClickListener(position -> {
+            Trailer trailer = trailerAdapter.getItem(position);
+            if (trailer != null){
+                Intent intent = null;
+                try {
+                    intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(Constants.APIConstants.BASE_YOUTUBE_URL + trailer.getKey()));
+                } catch (ActivityNotFoundException ex){
+                    showError(ex.getMessage());
+                }
+                startActivity(intent);
+            }
+        });
+        recyclerView.setAdapter(trailerAdapter);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    @Override
+    public void showLoading(boolean b) {
+
+    }
+
+    @Override
+    public void showError(String errorMessage) {
+        Toast.makeText(activity, errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+}
